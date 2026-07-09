5843fdc0de6f5c15337f06966665392f93989f84
Derek Oliveira <derek.foliveira@gmail.com>
2026-07-09 13:15:29 -0300

    Excessões

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -1,14 +1,19 @@
 import { Request, Response } from "express";
+import { AppError } from "../AppError.js";
 
 export class ProductsController {
-    index  (req: Request, res: Response) {
-          const { page, limit } = req.query;
-          res.send(`Página ${page} de ${limit}`);
+  index(req: Request, res: Response) {
+    const { page, limit } = req.query;
+    res.send(`Página ${page} de ${limit}`);
+  }
+
+  create(req: Request, res: Response) {
+    const { name, price } = req.body;
+
+    if (!name || !price) {
+      throw new AppError("Erro ao criar produto", 400);
     }
 
-    create (req: Request, res: Response) {
-          const { name, price } = req.body;
-
-  res.send({ name, price, user_id: req.user_id });
-    }
+    res.send({ name, price, user_id: req.user_id });
+  }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+
 import { routes } from "./routes";
+import { AppError } from "./AppError";
 
 const PORT = 3333;
 
@@ -9,6 +11,15 @@ app.use(express.json());
 
 app.use(routes);
 
+app.use((err: any, req: Request, res: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+  res.status(500).json({ message: "Internal server error" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-})
+});
